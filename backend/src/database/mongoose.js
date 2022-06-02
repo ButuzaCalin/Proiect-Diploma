0f16aaf6e5dd9c1dfbab23ba603394925cdb33bf
ButuzaCalin <67749327+ButuzaCalin@users.noreply.github.com>
2022-06-02 16:26:01 +0300

    Update mongoose.js

--- backend/src/database/mongoose.js
+++ backend/src/database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://calin:calin@cluster0.7eua3.mongodb.net/spotlight?retryWrites=true&w=majority"
+    "-"
   )
   .then(console.log("Conexiunea la BD OK"))
   .catch((e) => console.log(`Probleme la BD: ${e}`));
